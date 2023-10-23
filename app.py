9555f037c83027d5a48d2748ab251e80d04c132d
JoaquinRestahinoch <restahinoch@gmail.com>
2023-10-23 14:54:56 -0300

    a

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return
+from productos import productos
 
-@app.route('/redireccionar', methods=['GET'])
-def redireccionar():
-    nombre = request.args.get('nombre')
-    return redirect(url_for('saludo', nombre=nombre))
+#configuraciones del servidor/ secret permite generar una conexión segura
+app.secret_key = 'mysecretkey'
+@app.route('/')
+def Index():
+    return render_template('home.html')
+
+
+@app.route('/listar', methods=['GET'])
+def listar():
+    print(productos)
+    return jsonify(productos)
+    # return jsonify({'productos': productos})
+
+
+@app.route('/registrar_pedido', methods=['POST'])
+def registrar_pedido():
+    if request.method == 'POST':
+       
+        return redirect(url_for('Index'))
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 3000, debug = True)
+
