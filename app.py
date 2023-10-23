05262147aa36c86002060d1063866c26524fd2fb
JoaquinRestahinoch <restahinoch@gmail.com>
2023-10-23 18:49:38 -0300

    cambios endpoints

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, redirect
 from productos import productos
+app = Flask(__name__, template_folder='templates')  
 
-#configuraciones del servidor/ secret permite generar una conexión segura
-app.secret_key = 'mysecretkey'
 @app.route('/')
 def Index():
+    return render_template('inicio.html')
+
+@app.route('/home.html')
+def home():
     return render_template('home.html')
 
+@app.route('/redireccionar', methods=['POST'])
+def redireccionar():
 
-@app.route('/listar', methods=['GET'])
-def listar():
-    print(productos)
-    return jsonify(productos)
-    # return jsonify({'productos': productos})
+    return redirect('/home.html')
 
+@app.route('/categorias.html')
+def categorias():
+    return render_template('categorias.html')
 
-@app.route('/registrar_pedido', methods=['POST'])
-def registrar_pedido():
-    if request.method == 'POST':
-       
-        return redirect(url_for('Index'))
+@app.route('/category/<categoria>')
+def category(categoria):
 
+    productos_categoria = [producto for producto in productos if producto['categoria'] == categoria]
+    return render_template('category.html', categoria=categoria, productos=productos_categoria)
 
+@app.route('/contacto.html')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/terycond.html')
+def terycond():
+    return render_template('terycond.html')
+
+@app.route('/productos/<int:producto_id>')
+def mostrar_producto(producto_id):
+
+    producto = next((p for p in productos if p['id'] == producto_id), None)
+
+    if producto:
+        return render_template('productos.html', producto=producto)
+    else:
+        return "Producto no encontrado", 404
 
 if __name__ == '__main__':
-    app.run(port = 3000, debug = True)
-
+    app.run(port=3000, debug=True)
